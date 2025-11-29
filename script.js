4a33b8561efc1d6dc983d3de056414fdd5eb71e0
GKTechy <glapskrish@gmail.com>
2025-11-29 14:39:08 +0530

    Updated the full portfolio website

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sideMenu=document.querySelector('#sideMenu');
-
+const navbar=document.querySelector('nav');
+const navbarLinks=document.querySelector('nav ul');
 
 function openMenu(){
     sideMenu.style.transform='translateX(-16rem)';
@@ -9,3 +10,24 @@ function closeMenu(){
     sideMenu.style.transform='translateX(16rem)';
 }
 
+window.addEventListener('scroll',()=>{
+    if(scrollY>50){
+        navbar.classList.add('bg-white','bg-opacity-50', 'backdrop-blur-md', 'shadow-sm','dark:bg-darkTheme','dark:shadow-white/20');
+        navbarLinks.classList.remove('bg-white','bg-opacity-50',  'shadow-sm','dark:border','dark:border-white/50','dark:bg-transparent' );
+    }else{
+        navbar.classList.remove('bg-white','bg-opacity-50', 'backdrop-blur-md', 'shadow-sm','dark:bg-darkTheme','dark:shadow-white/20');
+        navbarLinks.classList.add('bg-white','bg-opacity-50',  'shadow-sm','dark:border','dark:border-white/50','dark:bg-transparent' );
+    }
+});
+
+
+
+function toggleMode(){
+    document.documentElement.classList.toggle("dark");
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    if (isDarkMode) {
+        localStorage.theme = "dark";
+    } else {
+        localStorage.theme = "light";
+    }   
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+ tailwind.config = {
+    theme: {
+      extend: {
+        gridTemplateColumns: {
+          auto: "repeat(auto-fit, minmax(200px, 1fr))",
+        },
+        fontFamily: {
+          Outfit: ["'Outfit', sans-serif"],
+          Ovo: ["'Ovo', serif"],
+        },
+        colors: {
+          lightHover: "#fcf4ff",
+          darkHover: "#2a004a",
+          darkTheme: "#1a001f",
+        },
+      },
+    },
+    darkMode:'selector'
+  }
